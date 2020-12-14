21d162f4557612a70a81e8540a5482e2079311db
Jay McConnell <jmmccon@amazon.com>
2020-12-14 10:23:34 -0800

    error message for dated pyenv

--- build/dev.sh
+++ build/dev.sh
@@ -2,7 +2,7 @@
 
 which pyenv &> /dev/null || { echo "pyenv must be installed. See https://github.com/pyenv/pyenv#installation" ; exit 1 ; }
 which pyenv-virtualenv &> /dev/null || { echo "pyenv-virtualenv must be installed. See https://github.com/pyenv/pyenv-virtualenv#installation" ; exit 1 ; }
-pyenv install 3.8.6 -s
+pyenv install 3.8.6 -s || { echo "Your pyenv does not have python version 3.8.6, you likely need to update pyenv." ; exit 1 ; }
 pyenv virtualenv 3.8.6 eks-quickstart-dev || true
 eval "$(pyenv init -)"
 pyenv shell eks-quickstart-dev
